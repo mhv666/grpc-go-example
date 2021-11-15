e9b0d8eee4fee098fcf8df61370530945bb9d07f
mhorrach <marti_horrach@hotmail.com>
2021-11-15 21:28:47 +0100

    register chat service into grpcserver

--- app/main/server.go
+++ app/main/server.go
@@ -13,8 +13,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
-
+	s := chat.Server{}
 	grpcserver := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcserver, &s)
 
 	if err := grpcserver.Serve(lis); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
