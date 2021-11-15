ee14d0e3e4ce88e30b5cb22da2c64db6948c59b2
mhorrach <marti_horrach@hotmail.com>
2021-11-15 21:19:37 +0100

    add chat.go and a generated protobuffer

--- app/main/proto/chat/chat.go
+++ app/main/proto/chat/chat.go
@@ -9,7 +9,7 @@ import (
 type Server struct {
 }
 
-func (s *Server) sayHello(ctx context.Context, message *Message) (*Message, error) {
-	log.Printf("Recived a message body from client: %s", message.Body)
-	return &Message{Body: "Hello from the Server"}, nil
+func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
+	log.Printf("Received message body from client: %s", message.Body)
+	return &Message{Body: "Hello From the Server!"}, nil
 }
